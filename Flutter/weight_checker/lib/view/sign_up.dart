9408257bb81ec6bf7f1ab52a2c7acf647d2e7c95
Kagiso <kagisomautla@gmail.com>
2021-07-17 17:33:03 +0200

    Update Imports

---
+++ Flutter/weight_checker/lib/view/sign_up.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Sign_Up extends StatefulWidget {
+  @override
+  _Sign_UpState createState() => _Sign_UpState();
+}
+
+class _Sign_UpState extends State<Sign_Up> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
