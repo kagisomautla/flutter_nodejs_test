c74d611dd4545e7a1fa96352f2886582b57cc48f
Kagiso <kagisomautla@gmail.com>
2021-07-17 19:16:34 +0200

    Database connection

---
+++ API/NodeJs/script.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+var app = require('express')();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const { response } = require('express');
+
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers','*');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'doshex'
+});
+
+connection.connect(function(error){
+    if(!!error){
+        console.log('There was an error connecting to the server.');
+    }else{
+        console.log('Successfully connected to the server.');
+    }
+});
+
+app.listen(3000, ()=>console.log('CORS-enabled and Express Server is running on localhost:3000'));
