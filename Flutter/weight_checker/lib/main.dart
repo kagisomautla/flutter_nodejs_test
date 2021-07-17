0d87dd49878613a2b5b292c7cbaf36f843454a0e
Kagiso <kagisomautla@gmail.com>
2021-07-17 17:55:37 +0200

    Update main.dart

--- Flutter/weight_checker/lib/main.dart
+++ Flutter/weight_checker/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weight_checker/view/history.dart';
 import 'package:weight_checker/view/home.dart';
+import 'package:weight_checker/view/login.dart';
 import 'package:weight_checker/view/wrapper.dart';
 
 void main() {
@@ -8,12 +9,9 @@ void main() {
     home: Wrapper(),
     debugShowCheckedModeBanner: false,
     routes: {
-      //This evaluation checks if a user has a token or not:
-      //If there isn't a token, a user isn't logged in, therefore they'll be routed to the authentication page
-      //If there is a token, the user is logged in, therefore they'll be routed to the landing page.
       '/wrapper': (context) => Wrapper(),
       '/homepage': (context) => Home(),
-      //'/driver_home': (context) => DriverHome(),
+      '/login': (context) => Login(),
       '/historypage': (context) => History(),
     },
   ));
