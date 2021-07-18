a0f80429278360837bed374734235a5b9f4c7cd9
Kagiso <kagisomautla@gmail.com>
2021-07-18 17:12:22 +0200

    Database connection

--- API/NodeJs/script.js
+++ API/NodeJs/script.js
@@ -1,12 +1,11 @@
 var mysql = require('mysql');
-var app = require('express')();
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const { response } = require('express');
+var express = require('express');
+var app = express();
 
+//Enables us to pass json objects
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); 
 
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers','*');
