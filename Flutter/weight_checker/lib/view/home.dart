ad6d76eebaa8bac10ef7d1815fe7c95df651010c
Kagiso <kagisomautla@gmail.com>
2021-07-19 05:46:00 +0200

    Update home.dart

--- Flutter/weight_checker/lib/view/home.dart
+++ Flutter/weight_checker/lib/view/home.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weight_checker/model/api_services.dart';
 import '../constants.dart';
+import '../nav_drawer.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,13 +14,70 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _isLoading;
-  int weight;
+  double weight;
+
   TextEditingController weightController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  _showMsg(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(msg),
+      duration: Duration(seconds: 2),
+      action: SnackBarAction(
+        label: '',
+        onPressed: () {},
+      ),
+    ));
+  }
+
+  saveWeight() async {
+    var dt = DateTime.now();
+    var dateFormat = DateFormat("yy-MM-dd");
+    String createdOn = dateFormat.format(dt);
+    print(createdOn);
+    int user_id;
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final String _url = '/save_weight';
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      user_id = localStorage.getInt('user_id');
+      print(user_id);
+
+      var data = {
+        'weight': weight.toString(),
+        'created_on': createdOn,
+        'user_id': user_id,
+      };
+
+      final response = await Api().postData(data, _url);
+      Map<String, dynamic> body = json.decode(response.body);
+
+      if (body['status'] == 'success') {
+        _showMsg('New weight added.');
+      } else {
+        _showMsg('Could not new weight');
+      }
+
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      drawer: Drawer(
+        elevation: 0.0,
+        child: NavDrawer(),
+      ),
       appBar: AppBar(
         title: Text(
           'Weight Check\'r',
@@ -36,34 +97,36 @@ class _HomeState extends State<Home> {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
-                child: TextFormField(
-                  controller: weightController,
-                  obscureText: true,
-                  validator: (value) => value.length < 8
-                      ? 'Password must contain at least 8 characters.'
-                      : null,
-                  decoration: textInputDecoration.copyWith(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(0),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: weightController,
+                    validator: (value) => value.length == 0
+                        ? _showMsg('Weight may not be empty')
+                        : null,
+                    decoration: textInputDecoration.copyWith(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(0),
+                        ),
                       ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.grey[300],
+                          width: 1.0,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey[400],
+                          width: 1.0,
+                        ),
+                      ),
+                      labelText: 'Enter your weight here',
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(0),
-                      ),
-                      borderSide: BorderSide(
-                        color: Colors.grey[300],
-                        width: 1.0,
-                      ),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400],
-                        width: 1.0,
-                      ),
-                    ),
-                    labelText: 'Enter your weight here',
                   ),
                 ),
               ),
@@ -82,11 +145,18 @@ class _HomeState extends State<Home> {
                               MaterialStateProperty.all<Color>(Colors.black),
                         ),
                         onPressed: () async {
-                          //Action what should happened when button is clicked
-                          // if (_formKey.currentState.validate()) {
-                          //   _login();
-                          // }
-                          Navigator.popAndPushNamed(context, '/homepage');
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              weight =
+                                  double.parse(weightController.text.trim());
+                            });
+
+                            if (weight <= 0) {
+                              _showMsg('Weight must be greater than 0.');
+                            } else {
+                              saveWeight();
+                            }
+                          }
                         },
                         child: Container(
                           padding: const EdgeInsets.all(5.0),
