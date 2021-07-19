f74797fdc7ef75411a483a9537a6276615c7404a
Kagiso <kagisomautla@gmail.com>
2021-07-19 05:45:20 +0200

    Update main.dart

--- Flutter/weight_checker/lib/main.dart
+++ Flutter/weight_checker/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:weight_checker/view/history.dart';
 import 'package:weight_checker/view/home.dart';
@@ -5,6 +6,7 @@ import 'package:weight_checker/view/login.dart';
 import 'package:weight_checker/view/wrapper.dart';
 
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(MaterialApp(
     home: Wrapper(),
     debugShowCheckedModeBanner: false,
@@ -15,4 +17,13 @@ void main() {
       '/historypage': (context) => History(),
     },
   ));
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
