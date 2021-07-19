9f0fb5da90a9e12fcd5e55d6b2d44f3632cb3943
Kagiso <kagisomautla@gmail.com>
2021-07-19 05:44:58 +0200

    Update constants.dart

--- Flutter/weight_checker/lib/constants.dart
+++ Flutter/weight_checker/lib/constants.dart
@@ -30,35 +30,6 @@ const textInputDecoration = InputDecoration(
   ),
 );
 
-const paymentsForm = InputDecoration(
-  labelText: 'First Name',
-  labelStyle: TextStyle(
-    fontFamily: 'Righteous',
-    fontSize: 15,
-    color: Colors.black,
-  ),
-  fillColor: Colors.white,
-  filled: true,
-  hintStyle: TextStyle(
-    fontFamily: 'Righteous',
-    fontSize: 15,
-    fontWeight: FontWeight.bold,
-  ),
-  enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.grey, width: 1.0),
-  ),
-  errorStyle: TextStyle(
-    fontFamily: 'Righteous',
-    fontSize: 15,
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.black, width: 1.0),
-  ),
-  focusedErrorBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.black, width: 1.0),
-  ),
-);
-
 var universalTextStyle = TextStyle(
   fontSize: 15,
   fontFamily: 'Righteous',
